e3d058061801cbe69650c8512fc6d0b7c71975b9
suraj87899 <surajsharma87899@gmail.com>
2025-01-27 15:51:57 +0530

    new

--- Function/mainf.cpp
+++ Function/mainf.cpp
@@ -1,2 +1,17 @@
 // Function?
 
+#include<iostream>
+using namespace std;
+
+void printAdd(int a,int b){
+    int x =a+b;
+    cout<<"The sum is:"<<x<<endl;
+}
+int main(){
+    int a =10;
+    int b=20;
+    printAdd(a,b);
+
+
+    return 0;
+}
